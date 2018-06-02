2424dd5ec2b4655e7e19c1b745136de7636c253b
sureshkumarm8 <sureshkumarm8@gmail.com>
2018-06-02 18:47:24 +0530

    Sandwich app updated with working code. #1 Initialize views #2 Updated views #3 JSON Util file updated

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -4,16 +4,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
 
+import java.util.List;
+
 public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
+    private Sandwich sandwich;
+    TextView tvPlaceofOrigin, tvDescription, tvAlsoNameas, tvIngredients;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +26,10 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
 
         ImageView ingredientsIv = findViewById(R.id.image_iv);
+        tvPlaceofOrigin= findViewById(R.id.origin_tv);
+        tvDescription= findViewById(R.id.description_tv);
+        tvAlsoNameas= findViewById(R.id.also_known_tv);
+        tvIngredients= findViewById(R.id.ingredients_tv);
 
         Intent intent = getIntent();
         if (intent == null) {
@@ -36,7 +45,7 @@ public class DetailActivity extends AppCompatActivity {
 
         String[] sandwiches = getResources().getStringArray(R.array.sandwich_details);
         String json = sandwiches[position];
-        Sandwich sandwich = JsonUtils.parseSandwichJson(json);
+        sandwich = JsonUtils.parseSandwichJson(json);
         if (sandwich == null) {
             // Sandwich data unavailable
             closeOnError();
@@ -57,6 +66,23 @@ public class DetailActivity extends AppCompatActivity {
     }
 
     private void populateUI() {
-
+        // Retrieving private data from Sandwich model
+        tvPlaceofOrigin.setText(sandwich.getPlaceOfOrigin());
+        tvDescription.setText(sandwich.getDescription());
+        tvAlsoNameas.setText(getListAsString(sandwich.getAlsoKnownAs()));
+        tvIngredients.setText(getListAsString(sandwich.getIngredients()));
     }
+    private String getListAsString(List<String> strings) {
+        StringBuilder sb = new StringBuilder();
+        if (strings != null) {
+            for (int i = 0; i < strings.size(); i++) {
+                sb.append(strings.get(i));
+                if (i < strings.size() - 1) {
+                    sb.append(", ");
+                }
+            }
+        }
+        return sb.toString();
+    }
+
 }
